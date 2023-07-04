5c9f26586be725267872687a3ba2e615d8eb69b5
panbin <1764183867@qq.com>
2023-07-04 14:09:46 +0800

    调整项目结构

---
+++ plugin/DesignerData/DesignerData.cs
@@ -0,0 +1,44 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Data
+{
+	public class DesignerData
+	{
+		[BsonId]
+		public ObjectId CustomerId { get; set; }
+
+		public string Name { get; set; } = "";
+
+		public DesignerLayer Root { get; set; }
+
+		public DesignerData()
+		{
+			
+		}
+
+		public DesignerLayer CreateRootLayer(string layerName,string DesignerName)
+		{
+			
+			Name = DesignerName;
+			DesignerLayer Layer = new DesignerLayer(this, layerName);
+			Root = Layer;
+			return Layer;
+		}
+
+		public void SaveToLiteDB()
+		{
+			var success=LiteDbContext.Litedb.Designer.Update(this);
+		}
+	}
+
+	
+
+}
---
+++ plugin/DesignerData/DesignerLayer.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Data
+{
+	public class DesignerLayer
+	{
+		public int Level { get; set; } = 0;
+
+		public string Name { get; set; }
+
+		public string DataTableString { get; set; }
+
+		public List<DesignerMeta> metas { get; set; } = new List<DesignerMeta>();
+
+		private void CreateDefaultMeta(DesignerLayer rootLayer)
+		{
+			DesignerMeta root = new DesignerMeta()
+			{
+				Name = "ID",
+				Description = "null",
+
+			};
+			metas.Add(root);
+		}
+
+		public DesignerLayer(DesignerData data, string layerName)
+		{
+			Name = layerName;
+			//Data.TableName = layerName;
+			//创建默认元素
+			CreateDefaultMeta(this);
+		}
+
+		public DesignerLayer()
+		{
+
+		}
+	}
+
+
+}
