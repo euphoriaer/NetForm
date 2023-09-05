31281e8c2a7adf2c690e8f3582f6be0b88963ac1
panbin <48428549+euphoriaer@users.noreply.github.com>
2023-09-05 17:09:28 +0800

    Designer Node改造

--- main/Windows/Designer.Designer.cs
+++ main/Windows/Designer.Designer.cs
@@ -1,86 +1,99 @@
 ﻿namespace NetForm
 {
-	partial class Designer
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Designer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Designer));
-			toolStripButton3 = new ToolStripButton();
-			toolStrip1 = new ToolStrip();
-			toolStrip1.SuspendLayout();
-			SuspendLayout();
-			// 
-			// toolStripButton3
-			// 
-			toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
-			toolStripButton3.ImageTransparentColor = Color.Magenta;
-			toolStripButton3.Name = "toolStripButton3";
-			toolStripButton3.Size = new Size(36, 53);
-			toolStripButton3.Text = "保存";
-			toolStripButton3.TextImageRelation = TextImageRelation.ImageAboveText;
-			toolStripButton3.ToolTipText = "保存";
-			toolStripButton3.Click += toolStripButton3_Click;
-			// 
-			// toolStrip1
-			// 
-			toolStrip1.BackColor = SystemColors.ButtonShadow;
-			toolStrip1.GripMargin = new Padding(6);
-			toolStrip1.ImageScalingSize = new Size(32, 32);
-			toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton3 });
-			toolStrip1.Location = new Point(0, 0);
-			toolStrip1.Name = "toolStrip1";
-			toolStrip1.Size = new Size(1659, 56);
-			toolStrip1.TabIndex = 0;
-			toolStrip1.Text = "toolStrip1";
-			toolStrip1.ItemClicked += toolStrip1_ItemClicked;
-			// 
-			// Designer
-			// 
-			AutoScaleDimensions = new SizeF(96F, 96F);
-			AutoScaleMode = AutoScaleMode.Dpi;
-			ClientSize = new Size(1659, 854);
-			Controls.Add(toolStrip1);
-			Name = "Designer";
-			ShowIcon = false;
-			ShowInTaskbar = false;
-			Text = "Designer";
-			Load += Designer_Load;
-			KeyPress += Designer_KeyPress;
-			MouseDown += Designer_MouseDown;
-			MouseMove += Designer_MouseMove;
-			MouseUp += Designer_MouseUp;
-			toolStrip1.ResumeLayout(false);
-			toolStrip1.PerformLayout();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Designer));
+            toolStripButton3 = new ToolStripButton();
+            toolStrip1 = new ToolStrip();
+            netNodeEditor1 = new NetNodeLib.NetNodeEditor();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(36, 53);
+            toolStripButton3.Text = "保存";
+            toolStripButton3.TextImageRelation = TextImageRelation.ImageAboveText;
+            toolStripButton3.ToolTipText = "保存";
+            toolStripButton3.Click += toolStripButton3_Click;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = SystemColors.ButtonShadow;
+            toolStrip1.GripMargin = new Padding(6);
+            toolStrip1.ImageScalingSize = new Size(32, 32);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton3 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(1659, 56);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            toolStrip1.ItemClicked += toolStrip1_ItemClicked;
+            // 
+            // netNodeEditor1
+            // 
+            netNodeEditor1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            netNodeEditor1.BackColor = SystemColors.AppWorkspace;
+            netNodeEditor1.Location = new Point(0, 59);
+            netNodeEditor1.Name = "netNodeEditor1";
+            netNodeEditor1.Size = new Size(1659, 797);
+            netNodeEditor1.TabIndex = 1;
+            netNodeEditor1.Load += netNodeEditor1_Load;
+            // 
+            // Designer
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(1659, 854);
+            Controls.Add(netNodeEditor1);
+            Controls.Add(toolStrip1);
+            Name = "Designer";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            Text = "Designer";
+            Load += Designer_Load;
+            KeyPress += Designer_KeyPress;
+            MouseDown += Designer_MouseDown;
+            MouseMove += Designer_MouseMove;
+            MouseUp += Designer_MouseUp;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-		private ToolStripButton toolStripButton3;
-		private ToolStrip toolStrip1;
-	}
+        #endregion
+        private ToolStripButton toolStripButton3;
+        private ToolStrip toolStrip1;
+        private NetNodeLib.NetNodeEditor netNodeEditor1;
+    }
 }
--- main/Windows/Designer.cs
+++ main/Windows/Designer.cs
@@ -5,201 +5,220 @@ using DesignerMeta = Data;
 using UserControl = NetForm.Windows.DesignerMeta;
 using System.Windows.Controls;
 using Data;
+using NetNodeLib;
 
 namespace NetForm
 {
-	public partial class Designer : Form
-	{
-		public DesignerData designer;
-		public DesignerLayer layer;
-		public Designer(DesignerData designerData, DesignerLayer dataLayer)
-		{
-			designer = designerData;
-			layer = dataLayer;
-			InitializeComponent();
-			InitDesigner();
-		}
-		private List<UIControl> uIControls = new List<UIControl>();
-		private List<UserControl> userControl = new List<UserControl>();
+    public partial class Designer : Form
+    {
+        public DesignerData designer;
+        public DesignerLayer layer;
+        public Designer(DesignerData designerData, DesignerLayer dataLayer)
+        {
+            designer = designerData;
+            layer = dataLayer;
+            InitializeComponent();
+            InitDesigner();
+        }
+        private List<UIControl> uIControls = new List<UIControl>();
+        private List<UserControl> userControl = new List<UserControl>();
 
-		public int HorizontalInternal =200;
-		public int VerticalInternal = 80;
-		public Point BasePos;
-		/// <summary>
-		/// dx,dy
-		/// </summary>
-		public Action<int, int> MiddleMove;
-		private void InitDesigner()
-		{
-			BasePos = new Point(Width / 2 - 50, Height / 2 - 17);
-			MiddleMove += (x, y) =>
-			{
-				BasePos.X += x;
-				BasePos.Y += y;
-			};
-			DrawDesigner(designer.Root);
-		}
+        public int HorizontalInternal = 200;
+        public int VerticalInternal = 80;
+        public Point BasePos;
+        private void InitDesigner()
+        {
+            BasePos = new Point(Width / 2 - 50, Height / 2 - 17);
 
-		private void DrawDesigner(DesignerLayer layer)
-		{
-			var original = BasePos;
-			int Maxhight = layer.metas.Count * VerticalInternal;
-			int width = layer.Level * HorizontalInternal;
-			
-			var firstPos = new Point(original.X + width, original.Y + -Maxhight / 2);
-			
-			MiddleMove += (x, y) =>
-			{
-				//todo 判断是否选中层，选中层可拖拽
-				firstPos.X += x;
-				firstPos.Y += y;
-				
-			};
+            NetNode node = new NetNode();
+            node.Name = designer.Name;
+            var op = new LineOption()
+            {
+                Name = "AddField",
+                Clicked = () =>
+                {
+                    CreateField(node);
+                },
+            };
+            node.options.Add(op);
+            netNodeEditor1.DrawNode(node);
+        }
 
-			var CreateNewBtn = new UIButton();
-			for (int i = 0; i < layer.metas.Count; i++)
-			{
-				var item = layer.metas[i];
-				var btn=CreateMeta(new Point(firstPos.X, firstPos.Y + i * VerticalInternal), item, layer);
-				
-			}
+        private void CreateField(NetNode node,Action click=null)
+        {
+            string name = "";
+            if (UIInputDialog.InputStringDialog(this, ref name, desc: "字段名"))
+            {
+                var op = new LineOption()
+                {
+                    Name = name,
+                    Clicked = () =>
+                    {
+                        click?.Invoke();
+                    },
+                };
+                node.options.Add(op);
 
-			//绘制当前layer 的增加按钮
-			CreateNewBtn = CreateButton(new Point(firstPos.X, firstPos.Y + (layer.metas.Count) * VerticalInternal), "新字段", (o, e) =>
-			{
-				var newMeta = new Data.DesignerMeta()
-				{
-					Name = "New"
-				};
-				layer.metas.Add(newMeta);
-				CreateNewBtn.Location = new Point(firstPos.X, firstPos.Y + (layer.metas.Count) * VerticalInternal);
-				CreateMeta(new Point(firstPos.X, firstPos.Y + (layer.metas.Count - 1) * VerticalInternal), newMeta, layer);
-			});
-		}
+            }
+        }
 
-		private UIButton CreateButton(Point point, string name, EventHandler action)
-		{
-			var button = new Sunny.UI.UIButton();
-			button.Font = new Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point);
-			button.MinimumSize = new Size(1, 1);
-			button.Name = "newFormBtn";
-			button.Size = new Size(100, 35);
-			button.Location = point;
-			button.TabIndex = 0;
-			button.Text = name;
-			button.Click += action;
-			Controls.Add(button);
-			uIControls.Add(button);
-			return button;
-		}
+        private void DrawDesigner(DesignerLayer layer)
+        {
+            var original = BasePos;
+            int Maxhight = layer.metas.Count * VerticalInternal;
+            int width = layer.Level * HorizontalInternal;
 
-		private Windows.DesignerMeta CreateMeta(Point point, Data.DesignerMeta meta,DesignerLayer metaLayer)
-		{
+            var firstPos = new Point(original.X + width, original.Y + -Maxhight / 2);
 
-			var metaPanel = new Windows.DesignerMeta(meta);//todo 新层 layer ，点击修改Meta 数据
-			metaPanel.Location = point;
-			metaPanel.MainBtnClick += () =>
-			{
-				
-			};
-			metaPanel.ShowLayerClick += () =>
-			{
-				MessageBox.Show("todo 展示子层级面板");
-			};
-			metaPanel.DeleteLayerClick += () =>
-			{
-				MessageBox.Show("todo 删除子层");
-			};
-			metaPanel.NewLayerClick += () =>
-			{
-				DesignerLayer layer = new DesignerLayer();
-				layer.Level = metaLayer.Level + 1;
-				layer.Name = "New Layer";
-				meta.Son = layer;
-				DrawDesigner(layer);
-			};
-			if (meta.Son != null)
-			{
-				DrawDesigner(meta.Son);
-			}
-			Controls.Add(metaPanel);
-			userControl.Add(metaPanel);
-			return metaPanel;
-		}
+            var CreateNewBtn = new UIButton();
+            for (int i = 0; i < layer.metas.Count; i++)
+            {
+                var item = layer.metas[i];
+                var btn = CreateMeta(new Point(firstPos.X, firstPos.Y + i * VerticalInternal), item, layer);
 
-		private void Button_Click(object? sender, EventArgs e)
-		{
-			MessageBox.Show($"按下按钮");
-		}
+            }
 
-		private void Designer_Load(object sender, EventArgs e)
-		{
+            //绘制当前layer 的增加按钮
+            //CreateNewBtn = CreateButton(new Point(firstPos.X, firstPos.Y + (layer.metas.Count) * VerticalInternal), "新字段", (o, e) =>
+            //{
+            //    var newMeta = new Data.DesignerMeta()
+            //    {
+            //        Name = "New"
+            //    };
+            //    layer.metas.Add(newMeta);
+            //    CreateNewBtn.Location = new Point(firstPos.X, firstPos.Y + (layer.metas.Count) * VerticalInternal);
+            //    CreateMeta(new Point(firstPos.X, firstPos.Y + (layer.metas.Count - 1) * VerticalInternal), newMeta, layer);
+            //});
+        }
 
-		}
+        private UIButton CreateButton(Point point, string name, EventHandler action)
+        {
+            var button = new Sunny.UI.UIButton();
+            button.Font = new Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button.MinimumSize = new Size(1, 1);
+            button.Name = "newFormBtn";
+            button.Size = new Size(100, 35);
+            button.Location = point;
+            button.TabIndex = 0;
+            button.Text = name;
+            button.Click += action;
+            Controls.Add(button);
+            uIControls.Add(button);
+            return button;
+        }
 
-		private void Designer_KeyPress(object sender, KeyPressEventArgs e)
-		{
+        private Windows.DesignerMeta CreateMeta(Point point, Data.DesignerMeta meta, DesignerLayer metaLayer)
+        {
 
-			MessageBox.Show($"按下,{e.KeyChar}");
+            var metaPanel = new Windows.DesignerMeta(meta);//todo 新层 layer ，点击修改Meta 数据
+            metaPanel.Location = point;
+            metaPanel.MainBtnClick += () =>
+            {
 
-		}
-		private bool MiddleDown = false;
+            };
+            metaPanel.ShowLayerClick += () =>
+            {
+                MessageBox.Show("todo 展示子层级面板");
+            };
+            metaPanel.DeleteLayerClick += () =>
+            {
+                MessageBox.Show("todo 删除子层");
+            };
+            metaPanel.NewLayerClick += () =>
+            {
+                DesignerLayer layer = new DesignerLayer();
+                layer.Level = metaLayer.Level + 1;
+                layer.Name = "New Layer";
+                meta.Son = layer;
+                DrawDesigner(layer);
+            };
+            if (meta.Son != null)
+            {
+                DrawDesigner(meta.Son);
+            }
+            Controls.Add(metaPanel);
+            userControl.Add(metaPanel);
+            return metaPanel;
+        }
 
-		private void Designer_MouseDown(object sender, MouseEventArgs e)
-		{
-			if (e.Button == MouseButtons.Middle)
-			{
-				LastPos = e.Location;
-				MiddleDown = true;
-				Debug.WriteLine($"Down {e.Button}  {e.Clicks} {e.Location}  {e.X}  {e.Y}");
-			}
-		}
-		private Point LastPos;
-		private void Designer_MouseMove(object sender, MouseEventArgs e)
-		{
-			if (e.Button == MouseButtons.Middle)
-			{
-				var curPos = e.Location;
-				var dx = curPos.X - LastPos.X;
-				var dy = curPos.Y - LastPos.Y;
-				for (int i = 0; i < uIControls.Count; i++)
-				{
-					var curUI = uIControls[i];
-					curUI.Location = new Point(curUI.Location.X + dx, curUI.Location.Y + dy);
-				}
+        private void Button_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show($"按下按钮");
+        }
 
-				for (int i = 0; i < userControl.Count; i++)
-				{
-					var curUI = userControl[i];
-					curUI.Location = new Point(curUI.Location.X + dx, curUI.Location.Y + dy);
-				}
+        private void Designer_Load(object sender, EventArgs e)
+        {
 
-				if (MiddleMove != null)
-				{
-					MiddleMove(dx, dy);
-				}
+        }
 
-				LastPos = curPos;
-			}
+        private void Designer_KeyPress(object sender, KeyPressEventArgs e)
+        {
 
-		}
+            MessageBox.Show($"按下,{e.KeyChar}");
 
-		private void Designer_MouseUp(object sender, MouseEventArgs e)
-		{
+        }
+        private bool MiddleDown = false;
 
-		}
+        private void Designer_MouseDown(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == MouseButtons.Middle)
+            //{
+            //    LastPos = e.Location;
+            //    MiddleDown = true;
+            //    Debug.WriteLine($"Down {e.Button}  {e.Clicks} {e.Location}  {e.X}  {e.Y}");
+            //}
+        }
+        private Point LastPos;
+        private void Designer_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == MouseButtons.Middle)
+            //{
+            //    var curPos = e.Location;
+            //    var dx = curPos.X - LastPos.X;
+            //    var dy = curPos.Y - LastPos.Y;
+            //    for (int i = 0; i < uIControls.Count; i++)
+            //    {
+            //        var curUI = uIControls[i];
+            //        curUI.Location = new Point(curUI.Location.X + dx, curUI.Location.Y + dy);
+            //    }
 
-		private void toolStripButton3_Click(object sender, EventArgs e)
-		{
-			designer.SaveToLiteDB();
-			
-			//创建新表
-			this.DialogResult = DialogResult.OK;
-		}
+            //    for (int i = 0; i < userControl.Count; i++)
+            //    {
+            //        var curUI = userControl[i];
+            //        curUI.Location = new Point(curUI.Location.X + dx, curUI.Location.Y + dy);
+            //    }
 
-		private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-		{
+            //    if (MiddleMove != null)
+            //    {
+            //        MiddleMove(dx, dy);
+            //    }
 
-		}
-	}
+            //    LastPos = curPos;
+            //}
+
+        }
+
+        private void Designer_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            designer.SaveToLiteDB();
+
+            //创建新表
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void netNodeEditor1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
